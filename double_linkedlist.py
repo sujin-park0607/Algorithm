6160100fe4fc2086b988a89c1e2eb967250289ab
sujin-park0607 <goweg0607@gmail.com>
2022-02-04 13:36:00 +0900

    Add search & insert function

--- double_linkedlist.py
+++ double_linkedlist.py
@@ -1,5 +1,3 @@
-from numpy import double
-
 
 class Node:
     def __init__(self, data, prev=None, next=None):
@@ -12,6 +10,7 @@ class NodeMgmt:
         self.head = Node(data)
         self.tail = self.head
 
+    #데이터 추가 
     def insert(self, data):
         if self.head == None:
             self.head = Node(data)
@@ -25,15 +24,73 @@ class NodeMgmt:
             new.prev = node
             self.tail = new
 
+    #데이터 출력
     def desc(self):
         node = self.head
         while node:
             print(node.data)
             node = node.next
+            
+    #앞에서부터 데이터찾기
+    def search_from_head(self, data):
+        if self.head == None:
+            return False
+        
+        node = self.head
+        while node:
+            if node.data == data:
+                return node
+            else:
+                node = node.next
+        return False 
+    
+    #뒤에서부터 데이터찾기
+    def search_from_tail(self, data):
+        if self.tail == None:
+            return False
+        
+        node = self.tail
+        while node:
+            if node.data == data:
+                return node
+            else:
+                node = node.prev
+        return False
+
+    #특정 노드 앞에 데이터추가 
+    def insert_before(self,data, before_data):
+        if self.head == None:
+            self.head = Node(data)
+            return True
+        
+        node = self.tail
+        while node.data != before_data:
+            node = node.prev
+            if node == None:
+                return False
+        new = Node(data)
+        before_new = node.prev      
+        before_new.next = new 
+        new.prev = before_new 
+        new.next = node
+
+        node.prev = new 
+
+
+
+
     
 double_linked_list = NodeMgmt(0)
 
 for data in range(1,10):
     double_linked_list.insert(data)
 
+# double_linked_list.desc()
+# node_3 = double_linked_list.search_from_head(0)
+# print(node_3.data)
+
+# node_7 = double_linked_list.search_from_tail(7)
+# print(node_7.data)
+
+double_linked_list.insert_before(1.5,2)
 double_linked_list.desc()
