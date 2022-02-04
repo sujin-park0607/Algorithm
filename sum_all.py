52221d502895223a3d6babccdf4bf9bc0b0f1f18
sujin-park0607 <goweg0607@gmail.com>
2022-02-04 15:10:15 +0900

    시간복잡도

--- sum_all.py
+++ sum_all.py
@@ -1,12 +1,18 @@
+#시간복잡도 
+
 
 def sum_all(n):
     total = 0
     for num in range(1,n +1):
         total += num
     return total
+#시간복잡도 n 
+#O(n)
 
 def sum_all(n):
     return int(n * (n + 1)/2)
+#시간복잡도 1 
+# O(1)
 
 print(sum_all(100))
 print(sum_all(100))
