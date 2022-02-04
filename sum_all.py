2f54b413a59171aa366b15514857a0e8ef253d36
sujin-park0607 <goweg0607@gmail.com>
2022-02-04 15:09:38 +0900

    시간복잡도

---
+++ sum_all.py
@@ -0,0 +1,12 @@
+
+def sum_all(n):
+    total = 0
+    for num in range(1,n +1):
+        total += num
+    return total
+
+def sum_all(n):
+    return int(n * (n + 1)/2)
+
+print(sum_all(100))
+print(sum_all(100))
