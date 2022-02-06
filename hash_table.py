10acae7e4bb5dbef80b37bf628a220138917ee40
sujin-park0607 <goweg0607@gmail.com>
2022-02-06 23:39:43 +0900

    fix: hash_table

--- hash_table.py
+++ hash_table.py
@@ -14,6 +14,21 @@ def get_data(data):
     hash_address = hash_func(key)
     return hash_table[hash_address]
 
+hash_table = list([0 for i in range(8)])
+
+def get_key(data):
+    return hash(data)
+
+def hash_function(key):
+    return key % 8
+
+def save_data(data, value):
+    hash_address = hash_function(get_key(data))
+    hash_table[hash_address] = value
+    
+def read_data(data):
+    hash_address = hash_function(get_key(data))
+    return hash_table[hash_address]
 
 storage_data('Andy', '01055553333')
 storage_data('Dave', '01044443333')
