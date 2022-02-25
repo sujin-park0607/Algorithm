4d20ddddf3b5f27380d86242c49c6ae4650ff4a1
sujin-park0607 <goweg0607@gmail.com>
2022-02-26 01:15:16 +0900

    add

--- backjoon/유기농배추.py
+++ backjoon/유기농배추.py
@@ -12,20 +12,24 @@ def dsf(array):
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
     x, y = 0,0
+
     q = deque()
     q.append((x,y))
 
-    while True:
-        if 0 <= x < N:
-            x += 1
-        else: y+= 1
-
+    while q:
         x,y  = q.popleft()
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
+            if nx<0 or nx >= M or ny <0 or ny >= N:
+                continue
+            
             if ground[nx][ny] == 1:
-                
-                ground[x][y] = ground[nx][ny] + 1 
+                if ground[nx][ny] not in q:
+                    q.append((nx,ny))
+                    ground[x][y] = ground[nx][ny] + 1
+    return ground
+
+print(dsf(ground)) 
