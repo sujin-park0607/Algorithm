de7b01405d41a9306efcb604a3280fa331352138
sujin-park0607 <goweg0607@gmail.com>
2022-12-12 02:53:27 +0900

    feat: programmers 네트워크

--- programmers/43162.py
+++ programmers/43162.py
@@ -1,20 +1,38 @@
+def dfs(graph, start):
+    stack = [start]
+
+    while stack:
+        current = stack.pop()
+        
+        if not visited[current]:
+            visited[current] = True
+            stack.extend(graph[current])
 
 def solution(n, computers):
+
+    col = len(computers[0])
+    row = len(computers)
+
     graph = {i:[] for i in range(n)}
 
-    for i in range(computers[0]):
-        for j in range(computers):
-            if(computer[i][j] == 1):
-                if i==j:
-                    continue
-                else:
-                    graph[i] = graph[i].get() + [j]
-                    graph[j] = graph[j].get() + [i]
-    print(graph)
+    for i in range(row):
+        for j in range(col):
+            if computers[i][j] == 1 and i != j:
+                graph[i] = graph.get(i) + [j]
+    
+    global visited
+    visited = [False for i in range(n)]
+
+    result = 0
+    for i in range(n):
+        if not visited[i]:
+            result += 1
+            dfs(graph, i)
+    return result
 
 
 n = 3
 computers1 = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
 computers2 = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
 
-solution(n, computers1)
+print(solution(n, computers2))
